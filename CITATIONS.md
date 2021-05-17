01c749d86c5d3eb1467425b3a04e2b97dfa25ac5
christy lo <christylo2000@hotmail.com>
2021-05-17 16:29:45 -0700

    added rough code for cards still need to map the input to create cards

--- CITATIONS.md
+++ CITATIONS.md
@@ -4,3 +4,5 @@ Assignment 1:
 
 - https://www.w3schools.com/css/css_navbar_horizontal.asp
 - https://github.com/nwplus/web-dev-intro/tree/draft
+- https://stackoverflow.com/questions/54706080/generating-dynamic-html-cards-from-a-javascript-array
+- https://www.w3schools.com/howto/howto_css_cards.asp
--- script/logic.js
+++ script/logic.js
@@ -1,5 +1,5 @@
 let pictureArray = [];
-// adds inputted photo name and corresponding photo url to photo list
+// adds inputted photo name and corresponding photo url to photo list and creates card for the photo
 function addPhoto() {
     console.log("add photo");
     const imageName = document.getElementById('imageName').value;
@@ -10,6 +10,8 @@ function addPhoto() {
         console.log(jsonPhoto);
         pictureArray.push(jsonPhoto);
         console.log(pictureArray);
+        createCard(pictureArray);
+        console.log("created the cards");
     }
 }
 
@@ -20,6 +22,30 @@ function createJSONObject(imageName, imageURL) {
         url: imageURL
     };
     return JSON.stringify(imageObject);
+}
+
+// creates a photo card elements with array of JSON photos
+function createCard(pictureArray) {
+    const container = document.getElementById('container');
+    pictureArray.forEach((result, idx) => {
+        // create the card element
+        const card = document.createElement('div');
+        card.classList = 'card-body';
+
+        // construct the card content
+        const content = `
+        <div class="card">
+            <img src="../images/christy.png" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h4><b>${result.name}</b></h4>
+                <p>${result.url}</p>
+            </div>
+        </div>
+      `;
+
+        // appeand newl created card element to the container
+        container.innerHTML += content;
+    })
 }
 
 // clears the input fields of current text
