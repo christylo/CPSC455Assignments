1b348c1b4cc7cdd35c4b1033104bd53b184a4d2c
christy lo <christylo2000@hotmail.com>
2021-05-16 20:31:10 -0700

    created global array to store json strings of the pictures

--- CITATIONS.md
+++ CITATIONS.md
@@ -1,4 +1,6 @@
 # Sources Referenced
 
+Assignment 1:
+
 - https://www.w3schools.com/css/css_navbar_horizontal.asp
 - https://github.com/nwplus/web-dev-intro/tree/draft
--- script/logic.js
+++ script/logic.js
@@ -1,4 +1,4 @@
-// import { writeFileSync } from 'fs';
+let pictureArray = [];
 // adds inputted photo name and corresponding photo url to photo list
 function addPhoto() {
     console.log("add photo");
@@ -8,20 +8,16 @@ function addPhoto() {
     if (imageName && imageURL) {
         jsonPhoto = createJSONObject(imageName, imageURL);
         console.log(jsonPhoto);
-        // const path = "./pictures.json";
-        // try {
-        //     writeFileSync(path, jsonPhoto);
-        // } catch (err) {
-        //     console.error(err);
-        // }
+        pictureArray.push(jsonPhoto);
+        console.log(pictureArray);
     }
 }
 
 // takes image name and url, turns it into an object literal, then into a JSON string
 function createJSONObject(imageName, imageURL) {
     const imageObject = {
-        "name": imageName,
-        "url": imageURL
+        name: imageName,
+        url: imageURL
     };
     return JSON.stringify(imageObject);
 }
