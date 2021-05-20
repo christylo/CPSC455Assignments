aa4942c0a8aff7fdbe16d300fad1a6d97699b8dc
christy lo <christylo2000@hotmail.com>
2021-05-19 19:40:08 -0700

    creates cards for with inputted image name and description but still need to load the image url into the picture

--- script/logic.js
+++ script/logic.js
@@ -4,9 +4,10 @@ function addPhoto() {
     console.log("add photo");
     const imageName = document.getElementById('imageName').value;
     const imageURL = document.getElementById('imageURL').value;
+    const imageDescription = document.getElementById('imageDescription').value;
     let jsonPhoto;
-    if (imageName && imageURL) {
-        jsonPhoto = createJSONObject(imageName, imageURL);
+    if (imageName && imageURL && imageDescription) {
+        jsonPhoto = createJSONObject(imageName, imageURL, imageDescription);
         console.log(jsonPhoto);
         pictureArray.push(jsonPhoto);
         console.log(pictureArray);
@@ -16,10 +17,11 @@ function addPhoto() {
 }
 
 // takes image name and url, turns it into an object literal, then into a JSON string
-function createJSONObject(imageName, imageURL) {
+function createJSONObject(imageName, imageURL, imageDescription) {
     const imageObject = {
         name: imageName,
-        url: imageURL
+        url: imageURL,
+        description: imageDescription
     };
     return JSON.stringify(imageObject);
 }
@@ -27,25 +29,26 @@ function createJSONObject(imageName, imageURL) {
 // creates a photo card elements with array of JSON photos
 function createCard(pictureArray) {
     const container = document.getElementById('container');
-    pictureArray.forEach((result, idx) => {
-        // create the card element
-        const card = document.createElement('div');
-        card.classList = 'card-body';
+    let addedPicture = pictureArray.slice(-1).pop();
+    let parsedJSONPicture = JSON.parse(addedPicture);
 
-        // construct the card content
-        const content = `
+    // create the card element
+    const card = document.createElement('div');
+    card.classList = 'card-body';
+
+    // construct the card content
+    const content = `
         <div class="card">
             <img src="../images/christy.png" alt="Avatar" style="width:100%">
-            <div class="container">
-                <h4><b>${result.name}</b></h4>
-                <p>${result.url}</p>
+            <div class="text-container">
+                <p><b>${parsedJSONPicture.name}</b></p>
+                <p>${parsedJSONPicture.description}</p>
             </div>
         </div>
-      `;
+    `;
 
-        // appeand newl created card element to the container
-        container.innerHTML += content;
-    })
+    // appeand newly created card element to the container
+    container.innerHTML += content;
 }
 
 // clears the input fields of current text
