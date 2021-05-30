39b9ea1e3a78f47c464ea67a578310297e87cfcc
christy lo <christylo2000@hotmail.com>
2021-05-30 13:23:35 -0700

    dev: ccreated styling from assignment 1

--- Assignment2/memory-collection/src/pages/Home.js
+++ Assignment2/memory-collection/src/pages/Home.js
@@ -1,5 +1,40 @@
 export default function Home() {
     return (
-        <h1>Home page</h1>
+        <div class="container">
+            <div class="intro">
+                <div id="left-content">
+                    <img id="Logo" src="../images/daisy.png" />
+                    <p id="Title">Memory Collection</p>
+                </div>
+                <p>Store photo memories in digital polaroids!</p>
+                <hr></hr>
+                <h2 class="heading">Add Polaroid</h2>
+                <div id="AddCard">
+                    <form>
+                        <label for="imageName">Image name: </label>
+                        <input class="textbox" type="text" id="imageName" name="imageName"></input>
+                        <br />
+                        <label for="imageDescription">Image description: </label>
+                        <input class="textbox" type="text" id="imageDescription" name="imageDescription"></input>
+                        <br />
+                        <label for="imageURL">Image URL: </label>
+                        <input class="textbox" type="text" id="imageURL" name="imageURL"></input>
+                        <br />
+                        <input class="button" type="button" value="Add Photo" onclick="addPhoto()"></input>
+                        <input class="button" type="button" value="Clear Fields" onclick="clearFields()"></input>
+                    </form>
+                </div>
+                <hr></hr>
+                <div class="flex-container">
+                    <div id="PolaroidHeader">
+                        <h2 class="heading">Polaroids</h2>
+                    </div>
+                    <div id="DeleteAllCardsButton">
+                        <button id="DeleteAllButton" class="button" onclick="deleteAll()">Delete All Polaroids</button>
+                    </div>
+                </div>
+                <div id="picture-container" class="highlights-grid"></div>
+            </div>
+        </div>
     );
 }
