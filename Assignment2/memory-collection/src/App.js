a9fb57a4ce60d79a80cf9171db251c7e5cd8bc57
christy lo <christylo2000@hotmail.com>
2021-05-30 12:55:16 -0700

    dev: created navbar routing

--- Assignment2/memory-collection/src/App.js
+++ Assignment2/memory-collection/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import './style/style.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <meta charset="utf-8" />
+      <title>Home - Memory Collection</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="shortcut icon" type="image/png" href="https://webstockreview.net/images/daisies-clipart-real-4.png" />
+      <link rel="stylesheet" href="../style/style.css" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap"
+        rel="stylesheet"
+      />
+      <Router>
+        <div>
+          <nav>
+            <li class="navbar">
+              <Link to="/" class="active">Home</Link>
+            </li>
+            <li class="navbar">
+              <Link to="/about">About</Link>
+            </li>
+          </nav>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 
-export default App;
