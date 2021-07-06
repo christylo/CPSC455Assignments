8fbd530e181fabb9a807e4b14605035375b7aee9
Christy Lo <christylo@Christys-MacBook-Air.local>
2021-07-05 23:39:42 -0700

    dev: backend connected with mongodb

---
+++ Assignment2/memory-collection/api/models/card-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Card = new Schema(
+    {
+        name: { type: String, required: true },
+        url: { type: String, required: true },
+        description: { type: String, required: true },
+        time: { type: Date, required: false },
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('cards', Card)
